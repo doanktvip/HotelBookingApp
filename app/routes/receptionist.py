571be52525f1f081a8b3d60ddfed2b24361f9253
Kirin200510 <hovandat2005@gmail.com>
2026-08-07 09:26:01 +0700

    create feature booking management and room management

---
+++ app/routes/receptionist.py
@@ -0,0 +1,79 @@
+from flask import Blueprint, render_template, request, abort, redirect, url_for
+from flask_login import login_required,current_user
+
+from app.services.search_service import SearchService
+from app.services.checkout_service import CheckoutService
+from app.utils import get_vn_time
+from app.models import Room, Booking, BookingStatus, RoomStatus, UserRole, RoomType, BookingDetail
+from app.extensions import db
+
+receptionist_bp = Blueprint('receptionist', __name__)
+
+@receptionist_bp.route('/recept', methods=['GET'])
+@login_required
+def recept():
+    if current_user.role != UserRole.RECEPTIONIST:
+        abort(403)
+    hotel_id = current_user.hotel_id
+    today = get_vn_time().date()
+
+    checkout_service = CheckoutService(db.session)
+    checkout_service.process_auto_checkout(hotel_id)
+
+    #lấy ds phòng của ks
+    rooms = Room.query.join(RoomType).filter(Room.is_active == True,RoomType.hotel_id == hotel_id).order_by(Room.floor, Room.room_number).all()
+    #lấy booking của ngày htai
+    bookings_today = db.session.query(BookingDetail.room_id).join(Booking).filter(
+        Booking.hotel_id == hotel_id,
+        Booking.status == BookingStatus.CONFIRMED,
+        Booking.check_in <= today,
+        Booking.check_out > today
+    ).all()
+    booked_room_ids = [b.room_id for b in bookings_today]
+
+    rooms_by_floor = {}
+    status_counts = {'AVAILABLE': 0, 'BOOKED': 0, 'OCCUPIED': 0, 'MAINTENANCE': 0}
+    floors = set()
+    for room in rooms:
+        actual_status = room.status.name
+        if actual_status == 'AVAILABLE':
+            #phòng có lịch đặt hôm nay
+            if room.id in booked_room_ids:
+                actual_status = 'BOOKED'
+        elif actual_status == 'BOOKED':
+            if room.id not in booked_room_ids:
+                actual_status = 'AVAILABLE'
+
+        room.actual_status = actual_status
+        if room.floor not in rooms_by_floor:
+            rooms_by_floor[room.floor] = []
+        rooms_by_floor[room.floor].append(room)
+        floors.add(room.floor)
+        status_counts[actual_status] += 1
+
+    floors = sorted(list(floors))
+    page = request.args.get('page', 1, type=int)
+    search = request.args.get('search', '').strip()
+    search_service = SearchService(db.session)
+    bookings_pagination = search_service.search_booking_in_recept(
+        hotel_id=hotel_id,
+        search_keyword=search,
+        page=page,
+        per_page=2
+    )
+    return render_template('room_management.html',rooms_by_floor=rooms_by_floor,status_counts=status_counts,floors=floors,bookings=bookings_pagination,today=today)
+
+
+@receptionist_bp.route('/update_booking_status/<int:booking_id>', methods=['POST'])
+@login_required
+def update_booking_status(booking_id):
+    if current_user.role != UserRole.RECEPTIONIST:
+        abort(403)
+
+    action = request.form.get('action')
+
+    checkout_service = CheckoutService(db.session)
+    checkout_service.update_status_at_counter(booking_id, current_user.hotel_id, action)
+
+    return redirect(url_for('receptionist.recept', tab='list'))
+
--- app/services/search_service.py
+++ app/services/search_service.py
@@ -1,5 +1,5 @@
-from sqlalchemy import func, exists,Float,case
-from app.models import Hotel, RoomType, SearchHistory, hotel_tags
+from sqlalchemy import func, exists,Float,case,or_
+from app.models import Hotel, RoomType, SearchHistory, hotel_tags,Booking, User
 from app.services import BaseService
 from app.services.ai_service import AIService
 from app.services.hotel_service import HotelService
@@ -122,3 +122,37 @@ class SearchService(BaseService):
         
         return final_hotels
 
+    def search_booking_in_recept(self, hotel_id, search_keyword='', page=1, per_page=10):
+        booking_query = Booking.query.join(User, Booking.user_id == User.id).filter(Booking.hotel_id == hotel_id)
+        search_keyword = search_keyword.strip()
+        if search_keyword:
+            search_id_str = search_keyword.upper().replace('BK-', '').strip()
+            if search_id_str.isdigit():
+                # Tìm theo ID HOẶC Tên HOẶC SĐT
+                booking_query = booking_query.filter(
+                    or_(
+                        Booking.id == int(search_id_str),
+                        User.username.ilike(f'%{search_keyword}%'),
+                        User.phone.ilike(f'%{search_keyword}%')
+                    )
+                )
+            else:
+                # Chỉ tìm theo Tên HOẶC SĐT
+                booking_query = booking_query.filter(
+                    or_(
+                        User.username.ilike(f'%{search_keyword}%'),
+                        User.phone.ilike(f'%{search_keyword}%')
+                    )
+                )
+        # Sắp xếp và phân trang
+        booking_query = booking_query.order_by(Booking.check_in.desc())
+        bookings_pagination = booking_query.paginate(page=page, per_page=per_page, error_out=False)
+
+        # Tính toán trạng thái "Đang ở"
+        for booking in bookings_pagination.items:
+            booking.is_currently_staying = any(
+                detail.room and detail.room.status.name == 'OCCUPIED'
+                for detail in booking.booking_details
+            )
+
+        return bookings_pagination
