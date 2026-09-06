af74fddf28bfe8e9ec1ded0c0b3bd9fe7bafd054
Nguyen Doan <nd21032005@gmail.com>
2026-09-06 17:12:57 +0700

    DDD-19

--- tests/services/test_checkout_service.py
+++ tests/services/test_checkout_service.py
@@ -2,8 +2,8 @@ import pytest
 from datetime import date, timedelta
 from werkzeug.exceptions import Forbidden, NotFound
 from app.services.checkout_service import CheckoutService
-from app.models import BookingStatus, RoomStatus
-
+from app.models import BookingStatus, RoomStatus,PaymentStatus
+from app.utils import get_vn_time
 @pytest.fixture
 def checkout_service(test_session):
     return CheckoutService(db_session=test_session)
@@ -27,7 +27,11 @@ def test_process_auto_checkout(test_app, checkout_service, sample_hotel, sample_
         for detail in sample_booking_details:
             assert detail.room.status == RoomStatus.AVAILABLE
 
-def test_update_status_at_counter_checkin(test_app, checkout_service, sample_hotel, sample_booking, sample_booking_details):
+def test_update_status_at_counter_checkin(test_app, checkout_service, sample_hotel, sample_booking, sample_booking_details, sample_payment):
+    sample_booking.check_in = get_vn_time().date()
+    if sample_booking.payment:
+        sample_booking.payment.status = PaymentStatus.SUCCESS
+    checkout_service.commit_or_rollback()
     with test_app.test_request_context():
         checkout_service.update_status_at_counter(sample_booking.id, sample_hotel.id, 'checkin')
         
@@ -35,6 +39,9 @@ def test_update_status_at_counter_checkin(test_app, checkout_service, sample_hot
             assert detail.room.status == RoomStatus.OCCUPIED
 
 def test_update_status_at_counter_checkout(test_app, checkout_service, sample_hotel, sample_booking, sample_booking_details):
+    for detail in sample_booking_details:
+        detail.room.status = RoomStatus.OCCUPIED
+    checkout_service.commit_or_rollback()
     with test_app.test_request_context():
         checkout_service.update_status_at_counter(sample_booking.id, sample_hotel.id, 'checkout')
         
